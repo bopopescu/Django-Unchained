be0381ffdae33f7a14fd5de3a8b4e7c53f0619c0
Huandari Lopez <hlopez137@gmail.com>
2014-11-19 23:25:05 -0800

    Forgot to revert custom_settings.py to previous settings

--- mysite/mysite/custom_settings.py
+++ mysite/mysite/custom_settings.py
@@ -7,8 +7,8 @@ DATABASES = {
    'default': {                                                                  
        'ENGINE': 'django.db.backends.postgresql_psycopg2',                       
        'NAME': 'mysite3',             # change it to your postgres database name
-       'USER': 'huandari',             # change it to the postgres database user    
-       'PASSWORD': 'chainsaw1',                 # change it to your own password      
+       'USER': 'tonyhlee',             # change it to the postgres database user    
+       'PASSWORD': '',                 # change it to your own password      
        'HOST': 'localhost',                                                      
        'PORT': '5432',                                                           
    }                                                                             
